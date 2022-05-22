479927970253da4a96cce9350c675e8f83cdc2ab
John Heinnickel <john.heinnickel@thermofisher.com>
2022-05-22 00:27:24 -0700

    Switch to using FTP service from lemon.itw

--- build_me.sh
+++ build_me.sh
@@ -3,12 +3,20 @@
 history="./hist_size.csv"
 
 basedir="$(basename `pwd`)"
-artifact="$(echo $basedir | sed 's/csd-genexus-//' | sed 's/pseudo-//')"
-state_now="$(cat state_now.dat)"
 
+# Strip csd-genexus- and psuedo- prefixes away from working directory name
+# to get the artifact name of project being build
+artifact="$(echo $basedir | sed 's/csd-genexus-//' | sed 's/pseudo-//')"
 echo "${artifact}"
+
+# Inspect checked in state file to understand what version we are pretending
+# to be building a component of the bundle for.
+state_now="$(cat state_now.dat)"
 echo "${state_now}"
 
+# Find the line for the component that will belong in the bundle whose
+# version was given by ${state_now} and pull it by FTP, simulating a genuine
+# build.
 for line in `grep "^${artifact}" "${history}"`
 do
 	echo "${line}"
@@ -16,10 +24,10 @@ do
 	echo "${match_to}"
 	if [[ ${match_to} == ${state_now} ]]
 	then
-		url="$(echo $line | awk -F, '{ print "http://nfs.ssfisilon2.itw/Valkyrie/AssayDev/TSDx/AssayDev/updates/"$1"_"$2"_"$3".deb" }')"
-		wget "${url}"
+		url="$(echo $line | awk -F, '{ print "ftp://lemon.itw/"$4"/AssayDev/TSDx/AssayDev/updates/"$1"_"$2"_"$3".deb" }')"
+		wget --ftp-user=anonymous "${url}"
 		file="$(basename ${url})"
-		echo $file > output.name
+		echo $file | tee output.name
 		exit 0
 	fi
 done
